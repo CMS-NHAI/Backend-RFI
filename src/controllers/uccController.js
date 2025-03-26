1e4d46dd81549a260daaed976c5a9f8a006e4a20
Your Name <bhawesh_bhanu@cms.co.in>
2025-03-26 16:25:05 +0530

    first commit

---
+++ src/controllers/uccController.js
@@ -0,0 +1,10 @@
+import { prisma } from "../config/prismaClient.js";
+import { STATUS_CODES } from "../constants/statusCodeConstants.js";
+import { RESPONSE_MESSAGES } from "../constants/responseMessages.js";
+
+
+/**
+ * Method : GET
+ * Params : @token
+ * Description : @uccListViaUser is use to get ucc list as per the user.
+*/
