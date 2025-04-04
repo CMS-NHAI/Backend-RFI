fcac4ef265f655cbd21e6eb6edb1c9662d206391
Your Name <bhawesh_bhanu@cms.co.in>
2025-04-05 00:53:44 +0530

    layers route

--- src/routes/rfiCreationRoutes.js
+++ src/routes/rfiCreationRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { validateToken } from "../middlewares/validate.js";
-import { agencyUserUccList, getRfiCategory,  getRfiItem,  getRfiSubCategory} from "../controllers/rfiCreationController.js";
+import { agencyUserUccList, getRfiCategory,  getRfiItem,  getRfiLayer,  getRfiSubCategory} from "../controllers/rfiCreationController.js";
 const router = express.Router()
 
 router.get('/OrgUserUccList', validateToken, agencyUserUccList);
 router.get('/rfiInspectionCategoryList', getRfiCategory);
 router.get('/rfiInspectionSubCategoryList', getRfiSubCategory);
 router.get('/rfiInspectionItemList', getRfiItem);
+router.get('/rfiInspectionLayerList',  getRfiLayer);
 
 export default router;
